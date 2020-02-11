daa14431078bd6f317993e4d965dcd55c857d55a
Iver Martinsen <iver.martinsen@outlook.com>
2020-02-11 15:31:00 +0100

    Edited chairs_per_person

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,12 @@
-f = open("reservations.csv")
+f = open("reservations")
 
 for reservation in f:
     name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
+    try:
+        chairs_per_person = 50 / int(number)
+    except ValueError as error:
+        print(error)
+    except ZeroDivisionError:
+        chairs_per_person = 0 / int(9)
+    
     print("{} will get {} chairs per person".format(name, chairs_per_person))
